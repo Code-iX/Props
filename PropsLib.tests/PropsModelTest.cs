3b4b0f838d2929feddb73d8f96fb4bc9b24d542c
Matthias Voigt <m.voigt@code-ix.de>
2015-05-23 20:35:59 +0200

    Empty tests added

---
+++ PropsLib.tests/PropsModelTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PropsLib.tests
+{
+    [TestClass]
+    public class PropsModelTest
+    {
+        [TestMethod]
+        public void TestPushLineWorking()
+        {
+        }
+    }
+}
---
+++ PropsLib.tests/PropsViewerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PropsLib.tests
+{
+    [TestClass]
+    public class PropsViewerTest
+    {
+        [TestMethod]
+        public void TestBasicScript()
+        {
+            // TODO Load basic.ppf and load into propsviewer. should have no errors
+        }
+
+
+    }
+}
